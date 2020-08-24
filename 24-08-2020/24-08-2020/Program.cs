2b3f41a67acc49376e83e30e884a542235dedd0e
Francisco <zfrancisconicolas@gmail.com>
2020-08-24 19:16:29 -0300

    Visual Studio github extension test

---
+++ 24-08-2020/24-08-2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _24_08_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
